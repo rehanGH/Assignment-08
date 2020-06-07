4abcc3fba0ca742654ff666050052cfea06f7691
Lamarr Baker <bakerlamarr@gmail.com>
2020-06-07 02:46:11 -0400

    fill and clear colors, drag and fill done

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import TableCell from "./TableCell";
 
 export default function TableRow(props) {
   let cells = [];
   
   for (let i = 0; i < props.columns; i++) {
-    cells.push(<TableCell color={props.color} applyColor={props.applyColor} />)
+    cells.push(<TableCell 
+      color={props.color} 
+      fill={props.fill} 
+      clear={props.clear}
+      applyColor={props.applyColor} 
+      fillAll={props.fillAll}
+      clearAll={props.clearAll} />)
   }
   return <tr>{cells}</tr>
 }

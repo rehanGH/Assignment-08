9b35cb3f8bbe08542d584cf9fe822391a29c866c
Rehan Khan <rehangh8924@gmail.com>
2020-06-07 23:29:42 -0400

    Updates

---
+++ precache-manifest.78ad4ff5c0b27781041573e8d0d40746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0b7fc7f3e00217e8d50bc615abbee9",
+    "url": "/Assignment-08/index.html"
+  },
+  {
+    "revision": "8218e3f0b00e6a37ba77",
+    "url": "/Assignment-08/static/css/main.514604c1.chunk.css"
+  },
+  {
+    "revision": "e8381219b7770592c559",
+    "url": "/Assignment-08/static/js/2.274856a5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Assignment-08/static/js/2.274856a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8218e3f0b00e6a37ba77",
+    "url": "/Assignment-08/static/js/main.e8d787c8.chunk.js"
+  },
+  {
+    "revision": "bbaac6941f1cfc35a625",
+    "url": "/Assignment-08/static/js/runtime-main.adcc0caa.js"
+  }
+]);
